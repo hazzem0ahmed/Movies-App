dc9660d4e299b7bb8523b28ace395f6799359810
Hazzem <hazzem.ahmed2000@gmail.com>
2025-11-22 18:24:30 +0200

    Movies App

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movies/core/app_colors.dart';
-import 'package:movies/presentation/widgets/elevated_button/elevated_button.dart';
 import 'package:movies/presentation/widgets/language/language_contract.dart';
-import 'package:movies/presentation/widgets/language/language_switch.dart';
 import 'package:movies/presentation/widgets/language/language_vm.dart';
-import 'package:movies/presentation/widgets/text_button/text_button.dart';
-import 'package:movies/presentation/widgets/text_field/text_filed.dart';
 import 'l10n/generated/app_localizations.dart';
 
 void main() {
