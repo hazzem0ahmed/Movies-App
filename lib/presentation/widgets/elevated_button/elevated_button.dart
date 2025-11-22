c6bd5a2e76093fc1f4832509741b02a645cd4573
Hazzem <hazzem.ahmed2000@gmail.com>
2025-11-22 18:21:33 +0200

    Movies App

---
+++ lib/presentation/widgets/elevated_button/elevated_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/app_colors.dart';
+
+class ElevatedButtonWidget extends StatelessWidget {
+  final String text;
+  ElevatedButtonWidget({super.key,required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: (){
+      //todo
+      },
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+
+          ),
+          backgroundColor: AppColors.yellow,
+          foregroundColor: AppColors.black,
+        ),
+       child: Row(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+           Text(text,style: TextStyle(
+             fontWeight: FontWeight.w400,
+             fontSize: 16,
+             color: AppColors.black,
+           ),),
+         ],
+       ));
+    }
+  }
+
---
+++ lib/presentation/widgets/text_button/text_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/app_colors.dart';
+
+class TextButtonWidget extends StatelessWidget {
+  final String text;
+  final FontWeight fontWeight;
+  TextButtonWidget({super.key,required this.text,this.fontWeight = FontWeight.w400,});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(onPressed: (){
+      //todo
+    }, child: Text(text,style: TextStyle(
+      fontSize: 14,
+      fontWeight: fontWeight,
+      color: AppColors.yellow,
+    ),));
+  }
+}
